72a36e2b6729075c0e06358232bd491b55636328
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-07-04 16:06:49 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
-
+/**
+ * Represents a user in the system.
+ */
 type User = Record<{
   id: string;
   username: string;
@@ -11,20 +13,33 @@ type User = Record<{
   resetToken: Opt<string>;
 }>
 
+/**
+ * Represents the payload for creating a new user.
+ */
 type UserPayload = Record<{
   username: string;
   email: string;
   password: string;
 }>
 
+// Initialize the user storage
 const userStorage = new StableBTreeMap<string, User>(0, 44, 1024);
 
-$query;
+/**
+ * Retrieve all users.
+ * @returns Result<Vec<User>, string> - The list of users on success, or an error message on failure.
+ */
+$query
 export function getUsers(): Result<Vec<User>, string> {
   return Result.Ok(userStorage.values());
 }
 
-$query;
+/**
+ * Get a user by their ID.
+ * @param id - The ID of the user to retrieve.
+ * @returns Result<User, string> - The user object on success, or an error message on failure.
+ */
+$query
 export function getUser(id: string): Result<User, string> {
   return match(userStorage.get(id), {
     Some: (user) => Result.Ok<User, string>(user),
@@ -32,7 +47,12 @@ export function getUser(id: string): Result<User, string> {
   });
 }
 
-$update;
+/**
+ * Register a new user.
+ * @param payload - The user payload containing username, email, and password.
+ * @returns Result<User, string> - The created user on success, or an error message on failure.
+ */
+$update
 export function registerUser(payload: UserPayload): Result<User, string> {
   const user: User = {
     id: uuidv4(),
@@ -44,7 +64,13 @@ export function registerUser(payload: UserPayload): Result<User, string> {
   return Result.Ok(user);
 }
 
-$query;
+/**
+ * Login a user.
+ * @param username - The username of the user.
+ * @param password - The password of the user.
+ * @returns Result<User, string> - The authenticated user on success, or an error message on failure.
+ */
+$query
 export function loginUser(username: string, password: string): Result<User, string> {
   const user = userStorage.values().find((u) => u.username === username && u.password === password);
   if (user) {
@@ -53,7 +79,12 @@ export function loginUser(username: string, password: string): Result<User, stri
   return Result.Err<User, string>('Invalid username or password');
 }
 
-$update;
+/**
+ * Delete a user.
+ * @param id - The ID of the user to delete.
+ * @returns Result<User, string> - The deleted user on success, or an error message on failure.
+ */
+$update
 export function deleteUser(id: string): Result<User, string> {
   return match(userStorage.remove(id), {
     Some: (deletedUser) => Result.Ok<User, string>(deletedUser),
@@ -61,7 +92,56 @@ export function deleteUser(id: string): Result<User, string> {
   });
 }
 
-$update;
+/**
+ * Update user profile information.
+ * @param id - The ID of the user to update.
+ * @param payload - The updated user payload containing username, email, and password.
+ * @returns Result<User, string> - The updated user on success, or an error message on failure.
+ */
+$update
+export function updateUser(id: string, payload: UserPayload): Result<User, string> {
+  return match(userStorage.get(id), {
+    Some: (user) => {
+      const updatedUser: User = {
+        ...user,
+        username: payload.username || user.username,
+        email: payload.email || user.email,
+        password: payload.password || user.password
+      };
+      userStorage.insert(id, updatedUser);
+      return Result.Ok<User, string>(updatedUser);
+    },
+    None: () => Result.Err<User, string>(`User with id=${id} not found`)
+  });
+}
+
+/**
+ * Change the password of a user.
+ * @param id - The ID of the user to update.
+ * @param newPassword - The new password for the user.
+ * @returns Result<User, string> - The updated user on success, or an error message on failure.
+ */
+$update
+export function changePassword(id: string, newPassword: string): Result<User, string> {
+  return match(userStorage.get(id), {
+    Some: (user) => {
+      const updatedUser: User = {
+        ...user,
+        password: newPassword
+      };
+      userStorage.insert(id, updatedUser);
+      return Result.Ok<User, string>(updatedUser);
+    },
+    None: () => Result.Err<User, string>(`User with id=${id} not found`)
+  });
+}
+
+/**
+ * Request a password reset for a user.
+ * @param email - The email of the user requesting the password reset.
+ * @returns Result<string, string> - The reset token on success, or an error message on failure.
+ */
+$update
 export function requestPasswordReset(email: string): Result<string, string> {
   const user = userStorage.values().find((u) => u.email === email);
   if (user) {
@@ -72,13 +152,18 @@ export function requestPasswordReset(email: string): Result<string, string> {
   return Result.Err<string, string>('User with the specified email not found');
 }
 
-$update;
+/**
+ * Reset the password of a user using a reset token.
+ * @param resetToken - The reset token for password reset.
+ * @param newPassword - The new password for the user.
+ * @returns Result<User, string> - The updated user on success, or an error message on failure.
+ */
+$update
 export function resetPassword(resetToken: string, newPassword: string): Result<User, string> {
   const user = userStorage.values().find((u) => match(u.resetToken, {
-    Some: (value:string) => value === resetToken,
+    Some: (value: string) => value === resetToken,
     None: () => false
   }));
-
 
   if (user) {
     const updatedUser = { ...user, password: newPassword, resetToken: Opt.None };
@@ -88,7 +173,10 @@ export function resetPassword(resetToken: string, newPassword: string): Result<U
   return Result.Err<User, string>('Invalid or expired reset token');
 }
 
-// Helper function to generate a random reset token
+/**
+ * Generate a random reset token.
+ * @returns string - The generated reset token.
+ */
 function generateResetToken(): string {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
@@ -103,13 +191,13 @@ function generateResetToken(): string {
 
 // a workaround to make uuid package work with Azle
 globalThis.crypto = {
-    getRandomValues: () => {
-        let array = new Uint8Array(32)
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
 
-        for (let i = 0; i < array.length; i++) {
-            array[i] = Math.floor(Math.random() * 256)
-        }
-
-        return array
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
     }
+
+    return array;
+  }
 }
